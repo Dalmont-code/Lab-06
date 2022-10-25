97f9a0fd747f5b25b509bdb22a11c47de6ecafa8
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2022-10-25 14:11:15 +0200

    Implementato la classe ResearchNode<N> per supportare la DFS (non ancora scritta)

---
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/api/ResearchNode.java
@@ -0,0 +1,39 @@
+package it.unibo.generics.graph.api;
+
+public class ResearchNode<N> extends GraphNode<N> {
+
+    private static final int INIT_COUNT = 1;
+    private static int counter = INIT_COUNT;
+
+    private int start = 0;
+    private int end = 0;
+    
+    public ResearchNode(N node) {
+        super(node);
+    }
+
+    public void resetSearchCounter() {
+        ResearchNode.counter = INIT_COUNT;
+    }
+
+    public int getStart() {
+        return this.start;
+    }
+
+    public int getEnd() {
+        return this.end;
+    }
+
+    public void setStart() {
+        if (start == 0) {
+            this.start = counter++;
+        }
+    }
+
+    public void setEnd() {
+        if (end == 0) {
+            this.end = counter++;
+        }
+    }
+    
+}
